2353ebffb21e12331ef3cb02da6bea1197d867a4
Saideep_2909 <72258638+S-A-I-V@users.noreply.github.com>
2024-09-19 14:34:00 +0530

    commit

--- coloredportal.py
+++ coloredportal.py
@@ -1,46 +1,48 @@
-from bisect import bisect_left
-t = int(input())
-results = []
+def solve():
+    t = int(input())
+    results = []
 
-for _ in range(t):
-    n, q = map(int, input().split())
-    portals = input().split()
+    for _ in range(t):
+        n, q = map(int, input().split())
+        portals = input().split()
+        
+        color_map = {'B': [], 'G': [], 'R': [], 'Y': []}
+        
+        for i in range(n):
+            c1, c2 = portals[i][0], portals[i][1]
+            color_map[c1].append(i + 1)
+            color_map[c2].append(i + 1)
+        
+        for _ in range(q):
+            x, y = map(int, input().split())
+            
+            if x == y:
+                results.append(0)
+                continue
+            
+            direct_connection = False
+            min_cost = abs(x - y)
+            
+            for color in portals[x - 1]:
+                if color in portals[y - 1]:
+                    direct_connection = True
+                    break
+            
+            if direct_connection:
+                results.append(min_cost)
+            else:
+                indirect_cost = float('inf')
+                
+                for color in portals[x - 1]:
+                    for city in color_map[color]:
+                        if city != x and any(color in portals[city - 1] for color in portals[y - 1]):
+                            indirect_cost = min(indirect_cost, abs(x - city) + abs(city - y))
+                
+                if indirect_cost < float('inf'):
+                    results.append(indirect_cost)
+                else:
+                    results.append(-1)
     
-    color_map = {'B': [], 'G': [], 'R': [], 'Y': []}
-    for i in range(n):
-        c1, c2 = portals[i][0], portals[i][1]
-        color_map[c1].append(i + 1)
-        color_map[c2].append(i + 1)
-    
-    for color in color_map:
-        color_map[color].sort()
-    
-    for _ in range(q):
-        x, y = map(int, input().split())
-        
-        if x == y:
-            results.append(0)
-            continue
-        
-        min_cost = abs(x - y)
-        found = False
-        
-        for color in portals[x - 1]:
-            if color in color_map:
-                positions = color_map[color]
-                pos = bisect_left(positions, y)
-                if pos < len(positions):
-                    min_cost = min(min_cost, abs(x - positions[pos]) + abs(positions[pos] - y))
-                    found = True
-                if pos > 0:
-                    min_cost = min(min_cost, abs(x - positions[pos - 1]) + abs(positions[pos - 1] - y))
-                    found = True
-        
-        if found:
-            results.append(min_cost)
-        else:
-            results.append(-1)
+    print('\n'.join(map(str, results)))
 
-print('\n'.join(map(str, results)))
-
-
+solve()
